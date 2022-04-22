d9c67b0dca528c713baf0fa6eba40e1b87b6381e
KoketsoMalatji <koketsosean209@gmail.com>
2022-04-22 09:48:53 -0700

    singly_linked_lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,9 @@
-message db "Hello, Holberton",10, 0
+message db "Hello, Holberton",10
 section .text
 global main
 main:
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, message
-	mov rdx, 18
+	mov rdx, 17
 	syscall

a33338b57d907f786cd2c8c6475e7ee5c53e5199
KoketsoMalatji <koketsosean209@gmail.com>
2022-07-06 15:22:06 -0700

    deletes a hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,7 +8,7 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	/* declarations */
-	hash_node_t *new_node, *freeable;
+	hash_node_t *loc, *freeable;
 	unsigned long int iterate;
 
 	/* check to see if there is a table */
@@ -17,16 +17,16 @@ void hash_table_delete(hash_table_t *ht)
 	/* iterate through and free it all, baby */
 	for (iterate = 0; iterate < ht->size; iterate++)
 	{
-		new = ht->array[iterate];
-		while (new_node)
+		loc = ht->array[iterate];
+		while (loc)
 		{
-			/* free contents without losing our new_node */
-			freeable = new_node;
-			new_node = new_node->next;
+			/* free contents without losing our loc */
+			freeable = loc;
+			loc = loc->next;
 			free(freeable->key);
 			free(freeable->value);
 			free(freeable);
-			/* start over unless !new_node */
+			/* start over unless !loc */
 		}
 	}
 	/* now free the table */

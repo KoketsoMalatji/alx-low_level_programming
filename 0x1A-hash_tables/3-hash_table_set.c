13faf4d777a4913ea2138cab726ed1095f5e3e03
KoketsoMalatji <koketsosean209@gmail.com>
2022-07-06 14:44:17 -0700

    adds an element

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,31 +11,31 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	int idx;
-	hash_node_t *new;
+	hash_node_t *node;
 
 	if (key == NULL || strlen(key) == 0 || ht == NULL)
 		return (0);
 
 	idx = key_index((const unsigned char *) key, ht->size);
 
-	new = ht->array[idx];
-	while (new != NULL)
+	node = ht->array[idx];
+	while (node != NULL)
 	{
-		if (strcmp(new->key, key) == 0)
+		if (strcmp(node->key, key) == 0)
 		{
-			free(new->value);
-			new->value = strdup(value);
+			free(node->value);
+			node->value = strdup(value);
 			return (1);
 		}
-		new = new->next;
+		node = node->next;
 	}
 
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
 		return (0);
-	new->key = strdup(key);
-	new->value = strdup(value);
-	new->next = ht->array[idx];
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = ht->array[idx];
 	ht->array[idx] = node;
 	return (1);
 }
